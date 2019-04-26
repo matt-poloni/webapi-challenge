75a3ea89ae21db869dfd63c1076614862cb6bc45
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-26 11:42:08 -0400

    Completes root GETs for projects & actions

--- routers/actions.js
+++ routers/actions.js
@@ -4,4 +4,14 @@ const db = require('../data/helpers/actionModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  db.get()
+    .then(actions => res.status(200).json(actions))
+    .catch(err => res.status(500).json({ error: 'The actions data could not be retrieved.' }))
+})
+
+router.post('/', (res, req) => {})
+router.put('/', (res, req) => {})
+router.delete('/', (res, req) => {})
+
 module.exports = router;
--- routers/projects.js
+++ routers/projects.js
@@ -4,4 +4,15 @@ const db = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  db.get()
+    .then(projects => res.status(200).json(projects))
+    .catch(err => res.status(500).json({ error: 'The projects data could not be retrieved.' }))
+})
+
+router.post('/', (req, res) => {})
+router.put('/', (req, res) => {})
+router.delete('/', (req, res) => {})
+router.get('/:id', (req, res) => {})
+
 module.exports = router;
