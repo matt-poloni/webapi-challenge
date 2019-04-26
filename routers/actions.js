1dca9608834be66083c7d13086e1af8a78359a48
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-04-26 12:14:54 -0400

    Should complete GETs by id and POSTs for project and actions

--- routers/actions.js
+++ routers/actions.js
@@ -1,8 +1,44 @@
 const express = require('express');
 
 const db = require('../data/helpers/actionModel');
+const dbProjects = require('../data/helpers/projectModel');
 
 const router = express.Router();
+
+// MIDDLEWARE
+
+const hasProjectID = (req, res, next) => {
+  !req.body.project_id
+    ? res.status(400).json({ error: 'Please provide a project id for the action.' })
+    : next();
+}
+
+const validProjectID = async (req, res, next) => {
+  const invalid = await dbProjects.get(req.body.project_id);
+  !invalid
+  ? res.status(400).json({ error: 'The specified project id does not exist. Please provide a valid project id.' })
+  : next();
+}
+
+const hasDescription = (req, res, next) => {
+  !req.body.description
+    ? res.status(400).json({ error: 'Please provide a description for the action.' })
+    : next();
+};
+
+const checkDescLength = (req, res, next) => {
+  req.body.description.length > 128
+    ? res.status(400).json({ error: 'Please provide a description that does not exceed a length of 128 characters.' })
+    : next();
+}
+
+const hasNotes = (req, res, next) => {
+  !req.body.notes
+    ? res.status(400).json({ error: 'Please provide notes for the action.' })
+    : next();
+};
+
+// ENDPOINTS
 
 router.get('/', (req, res) => {
   db.get()
@@ -10,8 +46,26 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ error: 'The actions data could not be retrieved.' }))
 })
 
-router.post('/', (res, req) => {})
+router.get('/:id', (req, res) => {
+  const actionID = req.params.id;
+  db.get(actionID)
+  .then(action => {
+    !action
+      ? res.status(404).json({ error: 'The action with the specified ID does not exist.' })
+      : res.status(200).json(action);
+  })
+    .catch(err => res.status(500).json({ error: 'The actions at the specified ID could not be retrieved.' }))
+})
+
+router.post('/', hasProjectID, validProjectID, hasDescription, checkDescLength, hasNotes, (req, res) => {
+  const newAction = req.body;
+  db.insert(newAction)
+    .then(insterted => res.status(201).json(insterted))
+    .catch(err => res.status(500).json({ error: 'The actions data could not be retrieved.' }))
+})
+
 router.put('/', (res, req) => {})
+
 router.delete('/', (res, req) => {})
 
 module.exports = router;
--- routers/projects.js
+++ routers/projects.js
@@ -4,15 +4,49 @@ const db = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+// MIDDLEWARE
+
+const hasName = (req, res, next) => {
+  !req.body.name
+    ? res.status(400).json({ error: 'Please provide a name for the project.' })
+    : next();
+}
+
+const hasDescription = (req, res, next) => {
+  !req.body.description
+    ? res.status(400).json({ error: 'Please provide a description for the project.' })
+    : next();
+}
+
+// ENDPOINTS
+
 router.get('/', (req, res) => {
   db.get()
     .then(projects => res.status(200).json(projects))
     .catch(err => res.status(500).json({ error: 'The projects data could not be retrieved.' }))
 })
 
-router.post('/', (req, res) => {})
+router.get('/:id', (req, res) => {
+  const projectID = req.params.id;
+  db.get(projectID)
+  .then(project => {
+    !project
+      ? res.status(404).json({ error: 'The project with the specified ID does not exist.' })
+      : res.status(200).json(project);
+  })
+    .catch(err => res.status(500).json({ error: 'The projects at the specified ID could not be retrieved.' }))
+})
+
+router.post('/', hasName, hasDescription, (req, res) => {
+  const newProject = req.body;
+  db.insert(newProject)
+    .then(insterted => res.status(201).json(insterted))
+    .catch(err => res.status(500).json({ error: 'The projects data could not be retrieved.' }))
+})
+
 router.put('/', (req, res) => {})
+
 router.delete('/', (req, res) => {})
-router.get('/:id', (req, res) => {})
+
 
 module.exports = router;
